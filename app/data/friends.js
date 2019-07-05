449465c16db2bdc4c8c459bbc11fef5b9b2ab13a
Dave Hawkins <dhawkins01@gmail.com>
2019-07-05 14:10:45 -0400

    added images and the friends.js data

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+// friends array that will hold the information for each possible match
+
+var friends = [
+    {
+        name: "Daniel",
+        image: "../public/images/Daniel.jpg",
+        scores: ["5", "3", "3", "1", "3", "2", "3", "5", "1", "2"]
+    },
+    {
+        name: "Dillon",
+        image: "../public/images/Dillon.jpg",
+        scores: ["3", "1", "2", "1", "5", "3", "1", "2", "2", "5"]
+    },
+    {
+        name: "Jane",
+        image: "../public/images/jane.jpg",
+        scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    },
+    {
+        name: "John",
+        image: "../public/images/John.jpg",
+        scores: ["5", "3", "2", "3", "4", "3", "2", "5", "2", "1"]
+    },
+    {
+        name: "Jorge",
+        image: "../public/images/Jorge.jpg",
+        scores: ["3", "2", "1", "5", "3", "2", "1", "1", "2", "2"]
+    },
+    {
+        name: "Julie",
+        image: "../public/images/Julie.jpg",
+        scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+    },
+    {
+        name: "Lauren",
+        image: "../public/images/Lauren.jpg",
+        scores: ["2", "2", "1", "3", "1", "3", "2", "5", "1", "3"]
+    },
+    {
+        name: "Rebecca",
+        image: "../public/images/Rebecca.jpg",
+        scores: ["5", "3", "3", "2", "5", "5", "5", "5", "4", "3"]
+    },
+    {
+        name: "Susie",
+        image: "../public/images/Susie.jpg",
+        scores: ["2", "3", "3", "2", "3", "4", "1", "5", "4", "2"]
+    },
+    {
+        name: "Tim",
+        image: "../public/images/Tim.jpg",
+        scores: ["1", "2", "3", "1", "4", "2", "5", "2", "3", "2"]
+    },
+]
